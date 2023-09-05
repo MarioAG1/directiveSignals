c51968fa33cda4e5a4a2197b0402010af601b49f
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-09-05 18:14:13 +0200

    Modificaciones, Set y Updates

--- src/app/signals/pages/property-page/property-page.component.ts
+++ src/app/signals/pages/property-page/property-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-property-page',
@@ -7,9 +8,49 @@ import { Component } from '@angular/core';
 })
 export class PropertyPageComponent {
 
+  public user = signal<User>(
+    {
+      id: 1,
+      email: 'george.bluth@reqres.in',
+      first_name: 'George',
+      last_name: 'Bluth',
+      avatar: 'https://reqres.in/img/faces/1-image.jpg'
+    }
+  )
 
-  onFieldUpdated(field: string, value: string) {
-    console.log(field, value);
+  onFieldUpdated(field: keyof User, value: string) {
+
+    // Obvia toda informacion anterior
+    // this.user.set({
+    //   ...this.user(),
+    //   [field]: value
+    // })
+
+    // Modificacio de la informacion anterior
+    this.user.update(current => {
+      return {
+        ...current,
+        [field]: value
+      }
+    })
+
+    // Cualquier modificacion dispara las actualizacion
+    // this.user.mutate(current => {
+    //   switch (field) {
+    //     case "email":
+    //       current.email = value
+    //       break;
+    //     case "first_name":
+    //       current.first_name = value
+    //       break;
+    //     case "last_name":
+    //       current.last_name = value
+    //       break;
+    //     case "id":
+    //       current.id = Number(value)
+    //       break;
+    //   }
+    // })
 
   }
 
